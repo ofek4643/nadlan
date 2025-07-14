1f2248bf2649bf31898a0ce8c1cb9611b44e1165
ofek4643 <ofekddia1@gmail.com>
2025-07-15 02:54:36 +0300

    render

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default ({ mode }) => {
-  // טען את קובצי env מתיקיית האב (root)
+  // טען את משתני הסביבה מהשורש
   const env = loadEnv(mode, path.resolve(__dirname, "."), "");
 
   return defineConfig({
     root: "./client",
     plugins: [react()],
     define: {
-      // העבר את משתני הסביבה לVite (לשימוש בקוד הלקוח)
-      "process.env": env,
+      // העבר רק את משתנה הסביבה שאתה צריך
       "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
     },
   });
