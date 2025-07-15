3a35639688842114eceeefa14e8e60a78c5f2e25
ofek4643 <ofekddia1@gmail.com>
2025-07-15 03:21:17 +0300

    server commit

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
   "https://nadlan-react1.onrender.com",
+  "https://nadlan-lxn4.onrender.com",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `ה-CORS policy לא מאפשר גישה מ-origin: ${origin}`;
-        return callback(new Error(msg), false);
+      if (!allowedOrigins.includes(origin)) {
+        return callback(new Error("CORS policy does not allow access"), false);
       }
       return callback(null, true);
     },
-    credentials: true,
+    credentials: true, // ← מאפשר עוגיות
   })
 );
 
