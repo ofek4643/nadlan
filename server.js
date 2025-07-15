74ca6006045677b5eb15e58cd6d808216cd187f2
ofek4643 <ofekddia1@gmail.com>
2025-07-15 04:13:24 +0300

    more update render

--- server.js
+++ server.js
@@ -183,8 +183,12 @@ const isAdmin = (req, res, next) => {
 app.post("/logout", (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
-    expires: new Date(0),
+    secure: process.env.NODE_ENV === "production", // ← אותו secure
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    expires: new Date(0), // או maxAge: 0
     path: "/",
+    // אם בלוג‑אין הוספת:
+    // domain: ".onrender.com",      // ← תוסיף גם כאן
   });
   res.status(200).json({ message: "התנתקת בהצלחה" });
 });
